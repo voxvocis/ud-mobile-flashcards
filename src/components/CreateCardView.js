37cb0bf85a86fd8039fa950acdeb968033f07123
Vox Vocis <thevoxvocis@gmail.com>
2018-02-26 22:58:52 +0100

    init create card & deck view

--- src/components/CreateCardView.js
+++ src/components/CreateCardView.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import { View, Text, Button, ScrollView } from 'react-native'
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
 import TextButton from './TextButton'
 import NotFound from './NotFound'
 
 /*
   New Question View
-  An option to enter in the question
-  An option to enter in the answer
-  An option to submit the new question
+  An option to enter in the question -DONE
+  An option to enter in the answer -DONE
+  An option to submit the new question -DONE
+
+  Submit the data and navigate back
 */
 
 export default class CreateCardView extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      question: '',
+      answer: '',
+    }
   }
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state
@@ -21,9 +27,62 @@ export default class CreateCardView extends Component {
     }
   }
 
+  submitQuestion = () => {
+    // Add question
+    // Navigate back
+    this.props.navigation.goBack()
+  }
+
   render() {
     return(
-      <NotFound />
+      <View style={{ flex: 1 }}>
+        <View style={{justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={styles.heading}> Add a new Question </Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder='Enter a question'
+            style={styles.titleInput}
+            onChangeText={(question) => this.setState({question})}
+            value={this.state.question}
+          />
+        </View>
+        <View>
+          <TextInput
+            placeholder='Enter an answer'
+            style={styles.titleInput}
+            onChangeText={(answer) => this.setState({answer})}
+            value={this.state.answer}
+          />
+        </View>
+        <Button
+           title="Submit"
+           onPress={this.submitQuestion}
+         />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    color: '#f4509e',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 60,
+    marginBottom: 40,
+  },
+  titleInput: {
+    color: '#f4509e',
+    fontSize: 15,
+    fontWeight: 'normal',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30,
+  }
+})
--- src/components/CreateDeckView.js
+++ src/components/CreateDeckView.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import { View, Text, Button, ScrollView } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
 import TextButton from './TextButton'
-import NotFound from './NotFound'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 /*
   New Deck View
-  An option to enter in the title for the new deck
-  An option to submit the new deck title
+  An option to enter in the title for the new deck -DONE
+  An option to submit the new deck title -DONE
+
+  Submit the data and navigate back
 */
 
-export default class CreateDeckView extends Component {
+class CreateDeckView extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      text: '',
+    }
   }
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state
@@ -20,9 +26,62 @@ export default class CreateDeckView extends Component {
     }
   }
 
+  submitCard = () => {
+    // Add question
+    const { text } = this.state
+    if (!text) {
+      return
+    }
+    console.log(this.props)
+    this.props.addDeck(text)
+    // Navigate back
+    this.props.navigation.goBack()
+  }
+
   render() {
     return(
-      <NotFound />
+      <View style={{ flex: 1 }}>
+        <View style={{justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={styles.heading}> What is the title of your new deck? </Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder='Enter a title'
+            style={styles.titleInput}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+          />
+        </View>
+        <Button
+           title="Submit"
+           onPress={this.submitCard}
+         />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    color: '#f4509e',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 60,
+    marginBottom: 40,
+  },
+  titleInput: {
+    color: '#f4509e',
+    fontSize: 15,
+    fontWeight: 'normal',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30,
+  }
+})
+
+export default connect(null, actions)(CreateDeckView)
